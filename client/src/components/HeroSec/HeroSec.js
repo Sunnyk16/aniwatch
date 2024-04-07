2ea84450d0b335ea081fafa11a7537d95d6e2ed9
Sunnyk16 <kumarsunny1246gmail.com>
2024-04-07 10:31:21 +0530

    ref: changed

--- client/src/components/HeroSec/HeroSec.js
+++ client/src/components/HeroSec/HeroSec.js
@@ -12,7 +12,7 @@ function HeroSec() {
             <div className="d-grid gap-2 d-md-flex justify-content-md-start">
               <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
               <button type="button" className="btn btn-outline-secondary btn-lg px-4">Default</button>
-              <button type="button" className="btn btn-outline-secondary btn-lg px-4">Default</button>
+              
             </div>
           </div>
           
