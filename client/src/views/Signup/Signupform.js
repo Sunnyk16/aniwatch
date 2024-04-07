530863f969763bb78bf4c630852dbf37f6940372
Sunnyk16 <kumarsunny1246gmail.com>
2024-04-07 12:25:25 +0530

    ref: sign up form updated

--- client/src/views/Signup/Signupform.js
+++ client/src/views/Signup/Signupform.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {React ,useState} from 'react'
+import {useState} from 'react'
 import {Link} from 'react-router-dom'
 import axios from 'axios'
 import {useNavigate} from "react-router-dom"
