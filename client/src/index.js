c9adc70ff336dbedb4a2fb86aba3104cae2299a1
Sunnyk16 <kumarsunny1246gmail.com>
2024-04-07 12:15:31 +0530

    ref: login file added

--- client/src/index.js
+++ client/src/index.js
@@ -1,46 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
-import './index.css';
+import "./index.css";
 
-import Home from './views/Home/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Home from "./views/Home/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
-import Login from './views/Login/Login.js';
-import Signup from './views/Signup/Signup.js';
+import Login from "./views/Login/Login.js";
+import Signup from "./views/Signup/SignUp.js";
 
 
-
-import Contact from './views/Contact/Contact';
-
+import Contact from "./views/Contact/Contact";
 
 const router = createBrowserRouter([
-
   {
     path: "/home",
-    element:<Home/>
+    element: <Home />,
   },
-
 
   {
     path: "/register",
-    element:<Signup/>
+    element: <Signup />,
   },
 
   {
     path: "/login",
-    element:<Login/>
+    element: <Login />,
   },
-
 
   {
     path: "/contact",
-    element:<Contact/>
+    element: <Contact />,
   },
-
- 
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
