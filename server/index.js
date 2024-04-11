368e02e52e7953a20c42c1897a1cfac7f80c243a
shreyaa-kar <shreyakar444@gmail.com>
2024-04-11 13:19:21 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import  mongoose  from "mongoose";
 import UserModel from "./models/User.js";
 import MovieModel from "./models/MoviesData.js";
 import Contact from "./models/Contact.js";
+import Dashboard from "./models/Dashboard.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -166,6 +167,48 @@ app.delete("/contacts/:id", async(req, res)=>{
       data: null
   })
 })
+
+// Dashboard API
+
+app.get("/totalhours",async (req,res)=>{
+  const{id} = req.params;
+
+  await Dashboard.onclick({_id: id})
+
+  res.json({
+    success: true,
+    message: "added successfully",
+  })
+  })
+
+  app.get("/totalmovies",async (req,res)=>{
+    const{id} = req.params;
+
+  await Dashboard.onclick({_id: id})
+
+    res.json({
+      success: true,
+      message: "added successfully",
+    })
+    })
+
+    
+    app.get("/totaladded",async (req,res)=>{
+      const{id} = req.params;
+
+  await Dashboard.onclick({_id: id})
+
+      res.json({
+        success: true,
+        message: "added successfully",
+      })
+      })
+
+      
+
+
+
+
 
  
 //database connection
