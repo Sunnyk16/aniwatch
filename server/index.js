22113da1a9997720f956d86228bbbe479490baef
shreyaa-kar <shreyakar444@gmail.com>
2024-04-11 13:20:06 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -182,10 +182,7 @@ app.get("/totalhours",async (req,res)=>{
   })
 
   app.get("/totalmovies",async (req,res)=>{
-    const{id} = req.params;
-
-  await Dashboard.onclick({_id: id})
-
+    
     res.json({
       success: true,
       message: "added successfully",
@@ -194,7 +191,6 @@ app.get("/totalhours",async (req,res)=>{
 
     
     app.get("/totaladded",async (req,res)=>{
-      
       res.json({
         success: true,
         message: "added successfully",
