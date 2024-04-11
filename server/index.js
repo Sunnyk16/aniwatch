358359d2de68b035ba055255e50ef3f20d410488
shreyaa-kar <shreyakar444@gmail.com>
2024-04-11 13:21:18 +0530

    added api's in dashboard

--- server/index.js
+++ server/index.js
@@ -195,6 +195,10 @@ app.get("/totalhours",async (req,res)=>{
 
     
     app.get("/totaladded",async (req,res)=>{
+      const{id} = req.params;
+
+  await Dashboard.onclick({_id: id})
+
       res.json({
         success: true,
         message: "added successfully",
