fdd396dd874358d01ad38fb66e3e1f0f7460e690
suchitathorat <suchitathorat73@gmail.com>
2024-04-11 18:48:02 +0530

    about page is added

---
+++ client/src/views/Aboutus/Aboutus.js
@@ -0,0 +1,348 @@
+import React from "react";
+import Navbar from "../../components/Navabar/Navbar";
+import Img1 from './img-1.jpg';
+import Img2 from './img-2.jpg'
+import Img3 from './img-3.jpg'
+import Img4 from './img-4.webp'
+import Img5 from './img-5.jpg'
+import './Aboutus.css'
+import Footer from "../../components/Footer/Footer";
+
+function Aboutus() {
+  return (
+    <div className="">
+      <div>
+        <Navbar />
+      </div>
+
+      <div class="container col-xl-10 col-xxl-8 px-4 py-5 ">
+        <div class="row align-items-center g-lg-5 py-5">
+          <div class="col-lg-7 text-center text-lg-start">
+            <h1 class="display-4 fw-bold lh-1 text-primary mb-3">
+              About Us
+            </h1>
+            <p class="col-lg-10 fs-4">
+             We belive in the ability to bring the world of the movies/story to the audience.
+            </p>
+          </div>
+
+
+
+          <div class="col-md-10 mx-auto col-lg-5">
+           <div className="imgslider"></div>          
+          </div>
+
+
+
+
+          <div className="our-mission">
+            <h1 className="heading">Our Mission</h1>
+            <p className="fs-4">
+            Our Mission is to provide all your favoruite Movies ,Dramas and all other content to you with the bestest quality.
+            We also give the assurance of providing new released shows for you as soon as it released.
+            </p>
+          </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+          <section className="py-3 py-md-5 py-xl-8">
+
+        <div className="container">
+          <div className="row justify-content-md-center">
+            <div className="col-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6">
+              <h1 className=" display-5  fw-bold text-primary mb-2  text-center mt-4">
+                Our Team
+              </h1>
+              <p className=" fs-4 mb-4  mb-md-5 text-center text-dark ">
+                With Our Team, you can expect personalized attention and
+                engaging designs.
+              </p>
+              <hr className="w-50 mx-auto mb-5 mb-xl-9 border-dark " />
+            </div>
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="d-flex justify-content-center   flex-wrap">
+
+
+            <div className=" col-md-5">
+              <div className="card  border-0 shadow ">
+                <img
+                  src={Img1}
+                  className=" card-img-top p-4 "
+                  alt="Flora Nyra"
+                />
+                <div className="card-body">
+                  <h5 className="card-title text-primary">Flora Nyra</h5>
+                  <p className="card-text mb-3 mt-4">
+                    Define the product vision and strategy and conduct user
+                    research to understand the needs of the target market.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+
+
+
+            <div className="col-md-5 ">
+              <div className="card  border-0 shadow">
+                <img
+                  src={Img1}
+                  className=" card-img-top p-4 "
+                  alt="Flora Nyra"
+                />
+                <div className="card-body">
+                  <h5 className="card-title text-primary">Flora Nyra</h5>
+                  <p className="card-text mb-3 mt-4">
+                    Define the product vision and strategy and conduct user
+                    research to understand the needs of the target market.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+
+
+
+            <div className="col-md-5 ">
+              <div className="card  border-0 shadow">
+                <img
+                  src={Img1}
+                  className=" card-img-top p-4 "
+                  alt="Flora Nyra"
+                />
+                <div className="card-body">
+                  <h5 className="card-title text-primary">Flora Nyra</h5>
+                  <p className="card-text mb-3 mt-4">
+                    Define the product vision and strategy and conduct user
+                    research to understand the needs of the target market.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+            <div className="col-md-5 ">
+              <div className="card  border-0 shadow">
+                <img
+                  src={Img1}
+                  className=" card-img-top p-4 "
+                  alt="Flora Nyra"
+                />
+                <div className="card-body">
+                  <h5 className="card-title text-primary">Flora Nyra</h5>
+                  <p className="card-text mb-3 mt-4">
+                    Define the product vision and strategy and conduct user
+                    research to understand the needs of the target market.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+
+            <div className="col-md-5 ">
+              <div className="card  border-0 shadow">
+                <img
+                  src={Img1}
+                  className=" card-img-top p-4 "
+                  alt="Flora Nyra"
+                />
+                <div className="card-body">
+                  <h5 className="card-title text-primary">Flora Nyra</h5>
+                  <p className="card-text mb-3 mt-4">
+                    Define the product vision and strategy and conduct user
+                    research to understand the needs of the target market.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+
+            <div className="col-md-5 ">
+              <div className="card  border-0 shadow">
+                <img
+                  src={Img1}
+                  className=" card-img-top p-4 "
+                  alt="Flora Nyra"
+                />
+                <div className="card-body">
+                  <h5 className="card-title text-primary">Flora Nyra</h5>
+                  <p className="card-text mb-3 mt-4">
+                    Define the product vision and strategy and conduct user
+                    research to understand the needs of the target market.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+            <div className="col-md-5 ">
+              <div className="card  border-0 shadow">
+                <img
+                  src={Img1}
+                  className=" card-img-top p-4 "
+                  alt="Flora Nyra"
+                />
+                <div className="card-body">
+                  <h5 className="card-title text-primary">Flora Nyra</h5>
+                  <p className="card-text mb-3 mt-4">
+                    Define the product vision and strategy and conduct user
+                    research to understand the needs of the target market.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+
+
+
+            <div className="col-md-5 ">
+              <div className="card  border-0 shadow">
+                <img
+                  src={Img1}
+                  className=" card-img-top p-4 "
+                  alt="Flora Nyra"
+                />
+                <div className="card-body">
+                  <h5 className="card-title text-primary">Flora Nyra</h5>
+                  <p className="card-text mb-3 mt-4">
+                    Define the product vision and strategy and conduct user
+                    research to understand the needs of the target market.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+
+
+
+            <div className="col-md-5 ">
+              <div className="card  border-0 shadow">
+                <img
+                  src={Img1}
+                  className=" card-img-top p-4 "
+                  alt="Flora Nyra"
+                />
+                <div className="card-body">
+                  <h5 className="card-title text-primary">Flora Nyra</h5>
+                  <p className="card-text mb-3 mt-4">
+                    Define the product vision and strategy and conduct user
+                    research to understand the needs of the target market.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+
+
+
+            
+
+
+
+
+
+
+            
+          </div>
+        </div>
+      </section>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+      </div>
+      <div>
+        <Footer/>
+          
+      </div>
+    </div>
+  );
+}
+
+export default Aboutus;
